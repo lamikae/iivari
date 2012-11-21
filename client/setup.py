890b040630c4861128d34d4e97691adb199ef64d
lamikae <mikael.lammentausta+github+@gmail.com>
2012-11-21 21:45:27 +0200

    Revert to static versioning in setup.py

--- client/setup.py
+++ client/setup.py
@@ -7,7 +7,8 @@ def read(fname):
 
 # Import package version. Relative path is searched
 # before system python packages.
-from iivari import __version__
+#from iivari import __version__
+__version__ = "1.4.1"
 
 setup(name="iivari-client",
       version=__version__,
